f0dcb1f25917884ef39e292a204c5e15e7e6cde2
Jaron <jayisaac6706@gmail.com>
2026-06-02 23:28:58 -0700

    logout route added

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authCtl from "../controllers/auth.controller.js";
-import { isLoggedIn, hasRole } from "../controllers/auth.controller.js";
+import { isLoggedIn, hasRole, logout } from "../controllers/auth.controller.js";
 import pageCtl from "../controllers/page.controller.js";
 
 const router = express.Router();
@@ -9,6 +9,8 @@ router.get("/", pageCtl.homePage);
 
 router.get("/login", authCtl.loginPage);
 router.post("/login", authCtl.login);
+
+router.get("/logout", logout);
 
 router.get("/register", authCtl.registerPage);
 router.post("/register", authCtl.register);
